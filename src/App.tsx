1449ac63320de8431884f1589fe1677c8e51d3a0
Lucas Danilo <lukastecno@gmail.com>
2024-03-08 00:16:17 -0300

    Project created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+
+    return (
+        <header>
+            <nav>
+                <div>
+                    <h1>Project Created!</h1>
+                </div>
+            </nav>
+        </header>
+    );
+
+}
